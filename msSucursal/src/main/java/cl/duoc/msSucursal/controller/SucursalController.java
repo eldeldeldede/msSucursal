4cb472dd4b8e6406d01b4178cafe2dbc7ad72cd6
bnmnnm <be.curinanco@duocuc.cl>
2026-06-09 12:36:33 -0400

    Documentacion

--- msSucursal/src/main/java/cl/duoc/msSucursal/controller/SucursalController.java
+++ msSucursal/src/main/java/cl/duoc/msSucursal/controller/SucursalController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.duoc.msSucursal.dto.SucursalDTO;
 import cl.duoc.msSucursal.model.Sucursal;
 import cl.duoc.msSucursal.service.SucursalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/v1/sucursales")
@@ -25,6 +27,10 @@ public class SucursalController {
     private SucursalService service;
 
     @GetMapping
+    @Operation(
+        summary = "Obtener la lista de sucursales registradas",
+        description = "Retorna la lista de sucursales registradas en el sistema del Rent a Car."
+    )
     public ResponseEntity<List<Sucursal>> listarSucursales(){
         try {
             List<Sucursal> listaSucursales = service.listar();
@@ -35,6 +41,10 @@ public class SucursalController {
     }
 
     @PostMapping
+    @Operation(
+        summary = "Registrar una nueva sucursal",
+        description = "Permite registrar una nueva sucursal en el sistema del Rent a Car."
+    )
     public ResponseEntity<Sucursal> guardarSucursal(Sucursal sucursal){
         try {
             Sucursal sucursalNueva = service.crearSucursal(sucursal);
@@ -45,6 +55,10 @@ public class SucursalController {
     }
 
     @GetMapping("/id/{id}")
+    @Operation(
+        summary = "Buscar sucursal por ID",
+        description = "Retorna los detalles de una sucursal específica por su ID."
+    )
     public ResponseEntity<Sucursal> buscarSucursal(@PathVariable Integer id){
         try {
             Sucursal sucursal = service.buscarSucursal(id);
@@ -55,6 +69,10 @@ public class SucursalController {
     }
 
     @PutMapping("/{id}")
+    @Operation(
+        summary = "Actualizar sucursal",
+        description = "Permite actualizar los detalles de una sucursal específica por su ID."
+    )
     public ResponseEntity<Sucursal> actualizarSucursal(@PathVariable Integer id,@RequestBody Sucursal sucursal){
         try {
             Sucursal sucursalActualizada = service.actualizarSucursal(id, sucursal);
@@ -65,6 +83,10 @@ public class SucursalController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+        summary = "Eliminar sucursal",
+        description = "Permite eliminar una sucursal específica por su ID."
+    )
     public ResponseEntity<Void> eliminarSucursal(@PathVariable Integer id){
         try {
             service.eliminarSucursal(id);
@@ -75,6 +97,10 @@ public class SucursalController {
     }
 
     @GetMapping("/dto/{id}")
+    @Operation(
+        summary = "Buscar sucursal por ID (DTO)",
+        description = "Retorna los detalles de una sucursal específica por su ID en formato DTO."
+    )
     public ResponseEntity<SucursalDTO> buscarDTO(@PathVariable Integer id){
         try {
             SucursalDTO dto = service.buscarDTO(id);
